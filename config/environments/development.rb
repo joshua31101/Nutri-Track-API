5a6e97faaa32c2f51cb44f632eb44c0860ae199d
Joshua Wang <joshuawang3110@gmail.com>
2019-04-11 22:35:41 -0400

    Add delete method as allowed cors request

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'localhost:4200', '127.0.0.1:4200'
-      resource '*', :headers => :any, :methods =>  [:get, :post, :options]
+      resource '*', :headers => :any, :methods =>  [:get, :post, :delete, :options]
     end
   end
 end
