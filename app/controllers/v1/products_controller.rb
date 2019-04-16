63e1400d58c2ba16734596d06db5e7f22902ab77
Joshua Wang <joshuawang3110@gmail.com>
2019-04-15 23:15:04 -0400

    Add product search route

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,7 +1,7 @@
 module V1
   class ProductsController < ApplicationController
     before_action :authorize_request
-    before_action :find_product
+    before_action :find_product, except: [:search]
 
     def show
       render json: @product, status: :ok
@@ -12,6 +12,11 @@ module V1
     def find_serving_size
       @serving_size = ServingSize.where(NDB_No: @product.ndb_number).first
       render json: { serving_size: @serving_size }, status: :ok
+    end
+
+    def search
+      @products = Product.search(params[:query], fields: [{long_name: :word_start}], limit: 10)
+      render json: { searched_products: @products }, status: :ok
     end
 
     private
