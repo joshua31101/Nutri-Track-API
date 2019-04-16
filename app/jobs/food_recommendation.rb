897ffcda8edeb2ce42af988a8cf15688e2159b69
Joshua Wang <joshuawang3110@gmail.com>
2019-04-16 00:16:57 -0400

    Fix error with duplicate key on creating product ingredient

--- app/jobs/food_recommendation.rb
+++ app/jobs/food_recommendation.rb
@@ -6,10 +6,7 @@ class FoodRecommendation < ActiveJob::Base
     Product.all[0..total_count].each do |product|
       recommended_product_ids = product.similar.to_a.select { |p| p.score >= product.score }.map(&:id)
       food_recommend = FoodRecommend.find_or_create_by(product_id: product.id)
-      food_recommend.update(
-        product_id: product.id,
-        recommended_product_ids: recommended_product_ids
-      )
+      food_recommend.update(recommended_product_ids: recommended_product_ids)
       p "#{count += 1}/#{total_count}: recommended product count => #{recommended_product_ids.count}"
     end
     p 'Finished!'
